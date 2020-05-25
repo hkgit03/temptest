f6a9a4b32a46b15dfdc5c11de18ef1076298b3fd
laerling <laerling@posteo.de>
2020-05-25 19:30:38 +0200

    fixup! Starting to write MUD in Python

---
+++ mud.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+class Player:
+    group = None
+    def __init__(name):
+        self.name = name
+
+alice    = Player("alice")
+bob      = Player("bob")
+charlie  = Player("charlie")
+dorothea = Player("dorothea")
+edgar    = Player("edgar")
+felix    = Player("felix")
+
+while True:
+    read(
